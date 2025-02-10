be21b1ce5aa93c9ab306118e284e6985092b1358
Abir BENZAAMIA <abirbenzaamia@Mhds-MacBook-Pro.local>
2025-02-10 23:03:46 +0100

    fix final issues

--- client/src/components/SurveyComponent.jsx
+++ client/src/components/SurveyComponent.jsx
@@ -313,7 +313,7 @@ const SurveyComponent = (step) => {
                     required
                     name="visited"
                     type="radio"
-                    value="2"
+                    value="3"
                     onChange={handleVisited}
                     className="relative size-5 appearance-none rounded-full border border-gray-300 bg-white before:absolute before:inset-1 before:rounded-full before:bg-white checked:border-indigo-600 checked:bg-indigo-600 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 disabled:border-gray-300 disabled:bg-gray-100 disabled:before:bg-gray-400 forced-colors:appearance-auto forced-colors:before:hidden [&:not(:checked)]:before:hidden"
                   />
