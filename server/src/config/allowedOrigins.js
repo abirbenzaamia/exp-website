36f1ccb6eef5c1e3036f823d63a3dd0eaa5712ab
Abir BENZAAMIA <abirbenzaamia@Mhds-MacBook-Pro.local>
2025-02-10 11:48:17 +0100

    fix cors

--- server/src/config/allowedOrigins.js
+++ server/src/config/allowedOrigins.js
@@ -1,6 +1,7 @@
 const allowedOrigins = [
-    "https://www.greensandbox.fr",
+    "https://greensandbox.fr",
     "https://1ab3-78-245-151-248.ngrok-free.app",
+    "https://exp-backend-zv8k.onrender.com",
     "http://localhost:3000",
     "http://localhost:5001"
   ];
